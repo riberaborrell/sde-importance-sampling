e837ce74ce61b50e6e86458686bd9247751ab314
eborrell <ribera.borrell@me.com>
2020-02-26 14:10:58 +0100

    #1 minor

--- plotting.py
+++ plotting.py
@@ -15,3 +15,14 @@ def plot_potential_and_gradient(X, V, dV):
 #TODO plot potential and gradient in 1D.
 
 #TODO plot tilted potential and basis functions
+
+
+def plot_trajectory(x, y):
+    plt.plot(x, y, 'r', label='EM solution path')
+    plt.xlabel('t', fontsize=16)
+    plt.ylabel('X', fontsize=16)
+    plt.xlim(right=60)
+    plt.ylim(bottom=-1.8)
+    plt.ylim(top=1.8)
+    plt.legend(loc='upper left', fontsize=8)
+    plt.show()
