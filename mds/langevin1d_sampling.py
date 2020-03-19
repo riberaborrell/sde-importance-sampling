25e69d33007d6cbfa6bc4807d9e598f92ce38d8e
eborrell <ribera.borrell@me.com>
2020-03-19 12:06:33 +0100

    setting for sampling and tilted sampling + computation of fht and qoi

--- mds/langevin1d_sampling.py
+++ mds/langevin1d_sampling.py
@@ -1,115 +1,168 @@
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 
-from plotting import plot_potential_and_gradient, \
-                     plot_trajectory
+from decorators import timer
+from plotting import Plot
 from tools import double_well_1d_potential, \
                   gradient_double_well_1d_potential
 
-parser = argparse.ArgumentParser(description='Brownian path simulation')
-parser.add_argument(
-    '--seed',
-    dest='seed',
-    type=int,
-    help='Set the seed for RandomState',
-)
-parser.add_argument(
-    '--beta',
-    dest='beta',
-    type=float,
-    default=10,
-    help='Set the parameter beta for the 1D MD SDE. Default: 1',
-)
-parser.add_argument(
-    '--xzero',
-    dest='xzero',
-    type=float,
-    default=-1.,
-    help='Set the value of the process at time t=0. Default: 1',
-)
-parser.add_argument(
-    '--target-set',
-    nargs=2,
-    dest='target_set',
-    type=float,
-    default=[0.9, 1.1],
-    help='Set the target set interval. Default: [0.9, 1.1]',
-)
-args = parser.parse_args()
+import os
+
+MDS_PATH = os.path.abspath(os.path.dirname(__file__))
+DATA_PATH = os.path.join(MDS_PATH, 'data')
+FIGURES_PATH = os.path.join(MDS_PATH, 'figures')
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Brownian path simulation')
+    parser.add_argument(
+        '--seed',
+        dest='seed',
+        type=int,
+        help='Set the seed for RandomState',
+    )
+    parser.add_argument(
+        '--beta',
+        dest='beta',
+        type=float,
+        default=5,
+        help='Set the parameter beta for the 1D MD SDE. Default: 5',
+    )
+    parser.add_argument(
+        '--xzero',
+        dest='xzero',
+        type=float,
+        default=-1.,
+        help='Set the value of the process at time t=0. Default: -1',
+    )
+    parser.add_argument(
+        '--target-set',
+        nargs=2,
+        dest='target_set',
+        type=float,
+        default=[0.9, 1.1],
+        help='Set the target set interval. Default: [0.9, 1.1]',
+    )
+    parser.add_argument(
+        '--num-trajectories',
+        dest='M',
+        type=int,
+        default=10**2,
+        help='Set number of trajectories to sample. Default: 100',
+    )
+    return parser
 
 
-# set random seed
-if args.seed:
-    np.random.seed(args.seed)
+@timer
+def main():
+    args = get_parser().parse_args()
 
-# check target set
-target_set_min = args.target_set[0]
-target_set_max = args.target_set[1]
-if target_set_min >= target_set_max:
-    #TODO raise error
-    print("The target set interval is not valid")
-    exit() 
+    # set random seed
+    if args.seed:
+        np.random.seed(args.seed)
 
-# grid x coordinate
-X = np.linspace(-2, 2, 100)
+    # check target set
+    target_set_min = args.target_set[0]
+    target_set_max = args.target_set[1]
+    if target_set_min >= target_set_max:
+        #TODO raise error
+        print("The target set interval is not valid")
+        exit() 
+    beta = args.beta
 
-# potential and gradient
-V = double_well_1d_potential(X)
-dV = gradient_double_well_1d_potential(X)
-plot_potential_and_gradient(X, V, dV)
-exit()
-
-# time interval, time steps and number of time steps
-tzero = 0
-T = 6*10 # 60 seconds
-N = 6*10**5 # 60 thousand time steps
-dt = (T - tzero) / N # 0,0001
-
-# Euler-Maruyama
-R = 1
-L = int(N/R)
-dt_coarse = R*dt
-t_coarse = np.linspace(tzero, T, L+1)
-
-# 1D MD SDE is dX_t = -grad V(X_t)dt + sqrt(2 beta**-1)dB_t, X_0 = x,
-beta = args.beta
-Xem = np.zeros(L+1)
-Xtemp = args.xzero
-Xem[0] = Xtemp
-
-# Brownian increments
-dB = np.sqrt(dt)*np.random.normal(0, 1, N)
-
-for j in np.arange(1, L+1):
-    # stop simulation if particle entered target set T
-    if (Xtemp > target_set_min and Xtemp < target_set_max):
-        break
+    # number of trajectories to sample
+    M = args.M
     
-    # compute the increments of the Brownian motion 
-    Binc = np.sum(dB[(j-1)*R: j*R])
+    # time interval, time steps and number of time steps
+    tzero = 0
+    T = 10**2
+    N = 10**6
+    dt = (T - tzero) / N
+    t = np.linspace(tzero, T, N+1)
+
+    # 1D MD SDE is dX_t = -grad V(X_t)dt + sqrt(2 beta**-1)dB_t, X_0 = x,
+    # preallocate EM solution
+    Xem = np.zeros((M, N+1))
+    Xem[:, 0] = args.xzero
+
+    # preallocate first hitting step/time array
+    FHs = np.zeros(M)
+    FHt = np.zeros(M)
     
-    # compute drift and diffusion coefficients
-    drift = - gradient_double_well_1d_potential(Xtemp)*dt_coarse
-    diffusion = np.sqrt(2/beta) * Binc
+    # preallocate observable of interest I
+    # for f=1, g=0, and therefore W = sigma t
+    I = np.ones(M)
 
-    # compute X_t
-    Xtemp = Xtemp + drift + diffusion
-    Xem[j] = Xtemp
+    # grid x coordinate
+    #X = np.linspace(-2, 2, 100)
 
+    # potential and gradient
+    #V = double_well_1d_potential(X)
+    #dV = gradient_double_well_1d_potential(X)
 
-if j == L:
-    print('The trajectory has NOT arrived to the target set!')
-else:
-    print('The trajectory HAS arrived to the target set!')
+    # plot potential and gradient 
+    #pl = Plot(file_name='potential_and_gradient', file_type='png')
+    #pl.potential_and_gradient(X, V, dV)
 
-    # final time and final number of steps
-    n = j*R
-    t = n*dt
+    for i in np.arange(M):
+        # Brownian increments
+        dB = np.sqrt(dt)*np.random.normal(0, 1, N)
     
-    Xem[j:L+1] = np.nan
-    print('Steps needed: {:8.2f} \n'.format(n))
-    print('Time needed: {:8.2f} \n'.format(t))
+        # initialize Xtemp
+        Xtemp = Xem[i, 0]
+        
+        for n in np.arange(1, N+1):
+            # compute Xem 
+            drift = - gradient_double_well_1d_potential(Xtemp)*dt
+            diffusion = np.sqrt(2/beta) * dB[n-1]
+            Xtemp = Xtemp + drift + diffusion
+            Xem[i, n] = Xtemp
+            
+            # check if we have arrived to the target set
+            if (Xtemp >= target_set_min and Xtemp <= target_set_max):
+                
+                # save first hitting time/step
+                FHs[i] = n 
+                FHt[i] = n * dt
 
-# plot trajectory
-plot_trajectory(t_coarse, Xem)
+                # compute quantity of interest at the fht
+                I[i] = np.exp(-beta * FHt[i])
+                
+                Xem[i, n:N+1] = np.nan
+                break
+
+    # plot last trajectory
+    pl = Plot(file_name='last_trajectory', file_type='png')
+    pl.trajectory(t, Xem[0])
+    
+    # sort out trajectories which have not arrived
+    FHt = np.array([t for t in FHt if t > 0])
+    I = np.array([x for x in I if x != 1])
+    
+    # compute mean and variance of tau
+    mean_tau = np.mean(FHt)
+    var_tau = np.var(FHt)
+    if mean_tau != 0:
+        re_tau = np.sqrt(var_tau) / mean_tau
+    else:
+        re_tau = np.nan
+
+    print('% trajectories which have arrived: {:2.4f}'.format(len(FHt) / M))
+    print('Expectation of tau: {:2.4f}'.format(mean_tau))
+    print('Variance of tau: {:2.4f}'.format(var_tau))
+    print('Relative error of tau: {:2.4f}'.format(re_tau))
+
+    # compute mean and variance of I
+    mean_I = np.mean(I)
+    var_I = np.var(I)
+    if mean_I != 0:
+        re_I = np.sqrt(var_I) / mean_I
+    else:
+        re_I = np.nan
+
+    print('Expectation of exp(-beta * tau): {:2.4e}'.format(mean_I))
+    print('Variance of exp(-beta * tau): {:2.4e}'.format(var_I))
+    print('Relative error of exp(-beta * tau): {:2.4e}'.format(re_I))
+
+
+if __name__ == "__main__":
+    main()
--- mds/langevin1d_tilted_sampling.py
+++ mds/langevin1d_tilted_sampling.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import argparse
 import numpy as np
 
-from plotting import Plot
 from decorators import timer
+from plotting import Plot
 from tools import gradient_double_well_1d_potential, \
                   gradient_bias_potential
 import os
 
 MDS_PATH = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.join(MDS_PATH, 'data')
+FIGURES_PATH = os.path.join(MDS_PATH, 'figures')
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Brownian path simulation')
@@ -25,15 +23,15 @@ def get_parser():
         '--beta',
         dest='beta',
         type=float,
-        default=10,
-        help='Set the parameter beta for the 1D MD SDE. Default: 1',
+        default=5,
+        help='Set the parameter beta for the 1D MD SDE. Default: 5',
     )
     parser.add_argument(
         '--xzero',
         dest='xzero',
         type=float,
         default=-1.,
-        help='Set the value of the process at time t=0. Default: 1',
+        help='Set the value of the process at time t=0. Default: -1',
     )
     parser.add_argument(
         '--target-set',
@@ -42,6 +40,13 @@ def get_parser():
         type=float,
         default=[0.9, 1.1],
         help='Set the target set interval. Default: [0.9, 1.1]',
+    )
+    parser.add_argument(
+        '--num-trajectories',
+        dest='M',
+        type=int,
+        default=10**2,
+        help='Set number of trajectories to sample. Default: 100',
     )
     return parser
 
@@ -69,44 +74,44 @@ def main():
     mus = bias_potential['mus']
     sigmas = bias_potential['sigmas']
     
-    # number of sampled trajectories
-    N_traj = 10
+    # number of trajectories to sample
+    M = args.M
     
     # time interval, time steps and number of time steps
     tzero = 0
-    T = 6*10**2 # 60 seconds
-    N = 6*10**4 # 6000 thousand time steps
-    dt = (T - tzero) / N # 0,01
+    T = 10**2
+    N = 10**6
+    dt = (T - tzero) / N
     t = np.linspace(tzero, T, N+1)
+    
+    # 1D MD SDE is dX_t = -grad V(X_t)dt + sqrt(2 beta**-1)dB_t, X_0 = x,
+    # preallocate EM solution
+    Xem = np.zeros((M, N+1))
+    Xem[:, 0] = args.xzero
 
     # preallocate first hitting step/time array
-    FHs = np.zeros(N_traj)
-    FHt = np.zeros(N_traj)
+    FHs = np.zeros(M)
+    FHt = np.zeros(M)
 
     # preallocate Girsanov Martingale
-    M_FHt = np.zeros(N_traj)
-    M_T = np.zeros(N_traj)
+    M_FHt = np.zeros(M)
+    M_T = np.zeros(M)
 
-    # preallocate observable of interest
+    # preallocate observable of interest I
     # for f=1, g=0, and therefore W = tau t
-    I = np.ones(N_traj)
+    I = np.ones(M)
 
-    # sample of N_traj trajectories 
-    for i in np.arange(N_traj):
+    # sample of M trajectories 
+    for i in np.arange(M):
         # Brownian increments
         dB = np.sqrt(dt)*np.random.normal(0, 1, N)
         
-        # preallocate EM solution
-        Xem = np.zeros(N+1) 
-        Xtemp = args.xzero
-        Xem[0] = Xtemp
+        # initialize Xtemp
+        Xtemp = Xem[i, 0]
 
         # preallocate martingale terms, M_t = e^(M1_t + M2_t)
         M1em = np.zeros(N+1)
         M2em = np.zeros(N+1)
-
-        # not been in target set yet
-        has_been_in_target_set = False
 
         for n in np.arange(1, N+1):
             # compute gradient of the bias potential evaluated at x
@@ -121,7 +126,7 @@ def main():
             drift = (- gradient_double_well_1d_potential(Xtemp) - dVbias)*dt
             diffusion = np.sqrt(2 / beta) * dB[n-1]
             Xtemp = Xtemp + drift + diffusion
-            Xem[n] = Xtemp
+            Xem[i, n] = Xtemp
 
             # evaluate the control drift
             Ut = -1 * dVbias / np.sqrt(2 / beta)
@@ -131,13 +136,8 @@ def main():
             M2em[n] = M2em[n-1] - (1/2)* (Ut**2) * dt 
 
             # check if we have arrived to the target set
-            if (has_been_in_target_set == False and
-                Xtemp >= target_set_min and 
-                Xtemp <= target_set_max):
+            if (Xtemp >= target_set_min and Xtemp <= target_set_max):
                 
-                # change flag
-                has_been_in_target_set = True
-
                 # save first hitting time/step
                 FHs[i] = n 
                 FHt[i] = n * dt
@@ -146,34 +146,43 @@ def main():
                 M_FHt[i] = np.exp(M1em[n] + M2em[n])
                 
                 # compute quantity of interest at the fht
-                I[i] = np.exp(-beta * FHt[i] * dt) * M_FHt[i] 
+                I[i] = np.exp(-beta * FHt[i]) * M_FHt[i] 
                 
-                Xem[n:N+1] = np.nan
+                Xem[i, n:N+1] = np.nan
                 break
 
-
-        # recall that 
-        # if not has_been_in_target_set:
-        #    FHs[i] = 0 
-        #    FHt[i] = 0
-        #    M_FHt[i] = 0
-        #    I[i] = 0
-       
-        #TODO: do I need this?
-        # compute Martingale at time T
-        #M_T[i] = np.exp(M1em[N] + M2em[N])
-    
     # plot last trajectory
-    pl = Plot(file_name='trajectory_tilted_dynamics', file_type='png')
-    pl.trajectory(t, Xem)
+    pl = Plot(file_name='last_tilted_trajectory', file_type='png')
+    pl.trajectory(t, Xem[0])
+
+    # sort out trajectories which have not arrived
+    FHt = np.array([t for t in FHt if t > 0])
+    I = np.array([x for x in I if x != 1])
+
+    # compute mean and variance of tau
+    mean_tau = np.mean(FHt)
+    var_tau = np.var(FHt)
+    if mean_tau != 0:
+        re_tau = np.sqrt(var_tau) / mean_tau
+    else:
+        re_tau = np.nan
+
+    print('% trajectories which have arrived: {:2.4f}'.format(len(FHt) / M))
+    print('Expectation of tau: {:2.4f}'.format(mean_tau))
+    print('Variance of tau: {:2.4f}'.format(var_tau))
+    print('Relative error of tau: {:2.4f}'.format(re_tau))
 
     # compute mean and variance of I
-    mean_I = np.mean(np.array([x for x in I if x > 0 ]))
-    var_I = np.var(np.array([x for x in I if x > 0 ]))
-    re_I = np.sqrt(var_I) / mean_I
-    print('Expectation of exp(-beta*tau*t)Mt: {:2.8f}'.format(mean_I))
-    print('Variance of exp(-beta*tau*t)Mt: {:2.8f}'.format(var_I))
-    print('Relative error of exp(-beta*tau*t)Mt: {:2.8f}'.format(re_I))
+    mean_I = np.mean(I)
+    var_I = np.var(I)
+    if mean_I != 0:
+        re_I = np.sqrt(var_I) / mean_I
+    else:
+        re_I = np.nan
+
+    print('Expectation of exp(-beta * tau): {:2.4e}'.format(mean_I))
+    print('Variance of exp(-beta * tau): {:2.4e}'.format(var_I))
+    print('Relative error of exp(-beta * tau): {:2.4e}'.format(re_I))
     
 
 if __name__ == "__main__":
