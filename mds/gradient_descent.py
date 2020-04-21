224276b9a175aeb281da5a9c00e3947f947a1072
eborrell <ribera.borrell@me.com>
2020-04-21 18:45:33 +0200

    minor in gradient descent module

--- mds/gradient_descent.py
+++ mds/gradient_descent.py
@@ -3,50 +3,78 @@ import sampling
 from metadynamics import get_a_from_metadynamics
 
 import numpy as np
+import os
+
+MDS_PATH = os.path.abspath(os.path.dirname(__file__))
+DATA_PATH = os.path.join(MDS_PATH, 'data')
 
 class gradient_descent:
     '''
     '''
-    def __init__(self, lr, epochs, m, M):
+    def __init__(self, lr, epochs, M):
         '''
         '''
-        self._m = m
-        self._a = np.zeros((epochs + 1, m))
-        self._mus = np.zeros(m)
-        self._sigmas = np.zeros(m)
-
         self._lr = lr
         self._epochs = epochs
-
         self._M = M
+
         self._losses = np.zeros(epochs + 1)
+        
+        self._m = None
+        self._as = None
+        self._mus = None
+        self._sigmas = None
+
+    def set_parameters_greedy(self, m):
+        epochs = self._epochs
+
+        self._m = m
+        self._as = np.zeros((epochs + 1, m))
+
+        a, mus, sigmas = get_a_from_metadynamics(
+            beta=2,
+            m=m,
+            J_min=-1.9,
+            J_max=0.9,
+        )
+        self._as[0] = a
+        self._mus = mus
+        self._sigmas = sigmas 
 
     def train_step(self, a):
         lr = self._lr
+        M = self._M
         mus = self._mus
         sigmas = self._sigmas
 
+        # initialize langevin_1d object
         samp = sampling.langevin_1d(
-            seed=1, 
-            beta=5,
-            xzero=-1,
-            target_set=[0.9, 1.1],
-            num_trajectories=1000, 
+            beta=2,
             is_drifted=True,
-            do_reweighting=False,
-            is_sampling_problem=False,
-            is_soc_problem=True,
         )
 
         # set a and coefficients of the ansatz functions
-        samp.get_a(a)
-        samp.get_v(mus, sigmas)
+        samp.set_bias_potential(a, mus, sigmas)
+
+        # set sampling and Euler-Majurama parameters
+        samp.set_sampling_parameters(
+            xzero=-1,
+            M=M,
+            target_set=[0.9, 1.1],
+            dt=0.001,
+            N=100000,
+        )
         
         # compute loss function and its gradient
-        samp.sample()
+        samp.sample_soc()
+        #samp.sample_soc_vectorized()
+
         samp.compute_statistics()
         loss = samp._mean_J
         grad_loss = samp._mean_gradJ
+
+        #print(a)
+        #print(loss)
             
         # Update parameters
         a = a - lr * grad_loss 
@@ -58,25 +86,25 @@ class gradient_descent:
         m = self._m
         epochs = self._epochs
         losses = self._losses
-
-        a, mus, sigmas = get_a_from_metadynamics(
-            beta=5,
-            m=m,
-            J_min=-1.9,
-            J_max=0.9,
-        )
-        self._a[0] = a
-        self._mus = mus
-        self._sigmas = sigmas 
-
+        
+        a = self._as[0]
         for epoch in np.arange(1, epochs + 1):
             print(epoch)
+
             # compute loss and do update parameters
             a, loss = self.train_step(a)
 
             # save loss 
-            self._losses[epoche - 1]
+            self._losses[epoch - 1] = loss
 
             # save parameters
-            self._a[epoche]
-        
+            self._as[epoch] = a
+
+
+    def save_statistics(self):
+        # save as and losses
+        np.savez(
+            os.path.join(DATA_PATH, 'gradient_descent_greedy.npz'),
+            a=self._as,
+            losses=self._losses,
+        )
--- mds/gradient_descent_test.py
+++ mds/gradient_descent_test.py
@@ -1,14 +1,85 @@
 import gradient_descent
+import sampling
+from plotting import Plot
+from potentials_and_gradients import double_well_1d_potential, \
+                                     double_well_1d_gradient
 
-soc = gradient_descent.gradient_descent(
-    lr=0.004,
-    epochs=10,
-    m=20,
-    M=1,
-)
-soc.gradient_descent()
-a = soc._a
-losses = soc._losses
+import argparse
+import numpy as np
 
-print(a[-1])
-print(losses[-1])
+import os
+
+MDS_PATH = os.path.abspath(os.path.dirname(__file__))
+FIGURES_PATH = os.path.join(MDS_PATH, 'figures')
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Gradient Descent')
+    parser.add_argument(
+        '--learning-rate',
+        dest='lr',
+        type=float,
+        default=0.004,
+        help='Set learning rate. Default: 0.004',
+    )
+    parser.add_argument(
+        '--epochs',
+        dest='epochs',
+        type=int,
+        default=10,
+        help='Set number of epochs. Default: 10',
+    )
+    parser.add_argument(
+        '--M',
+        dest='M',
+        type=int,
+        default=10**2,
+        help='Set number of trajectories to sample. Default: 100',
+    )
+    return parser
+
+def main():
+    args = get_parser().parse_args()
+    
+    # initialize gradient descent object
+    soc = gradient_descent.gradient_descent(
+        lr=args.lr,
+        epochs=args.epochs,
+        M=args.M,
+    )
+
+    soc.set_parameters_greedy(m=10)
+    soc.gradient_descent()
+    soc.save_statistics()
+    
+    a = soc._as[-1]
+    mus = soc._mus
+    sigmas = soc._sigmas
+    losses = soc._losses
+
+    print(a)
+    
+    
+    # initialize sample object
+    samp = sampling.langevin_1d(
+        beta=2,
+        is_drifted=True,
+    )
+    samp.set_bias_potential(a, mus, sigmas)
+
+    # plot tilted potential and gradient
+    X = np.linspace(-2, 2, 100)
+    V = double_well_1d_potential(X)
+    dV = double_well_1d_gradient(X)
+    Vbias = samp.bias_potential(X)
+    U = samp.control(X)
+    dVbias = samp.bias_gradient(U)
+
+    pl = Plot(
+        file_name='tilted_potential_and_gradient_gradient_descent',
+        file_type='png',
+        dir_path=FIGURES_PATH,
+    )
+    pl.tilted_potential_and_gradient(X, V, dV, Vbias, dVbias)
+
+if __name__ == "__main__":
+    main()
