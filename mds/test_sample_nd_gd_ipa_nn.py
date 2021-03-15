20abddfe301985bb3e589e6b1bcd19cc54267e2a
eborrell <ribera.borrell@me.com>
2021-03-15 17:50:20 +0100

    testing gd ipa with NN | nd and vectorized

--- mds/test_sample_nd_gd_ipa_nn.py
+++ mds/test_sample_nd_gd_ipa_nn.py
@@ -21,8 +21,8 @@ def get_parser():
         '--hidden-layer-dim',
         dest='hidden_layer_dim',
         type=int,
-        default=30,
-        help='Set dimension of the hidden layer. Default: 30',
+        default=10,
+        help='Set dimension of the hidden layer. Default: 10',
     )
     return parser
 
@@ -38,7 +38,7 @@ def main():
 
     # initialize control parametrization by a nn 
     d_in, d_1, d_out = args.n, args.hidden_layer_dim, args.n
-    model = TwoLayerNet(d_in, d_1, d_out).to(device)
+    model = TwoLayerNet(d_in, d_1, d_out)
 
     # define optimizer
     optimizer = optim.Adam(
@@ -58,12 +58,11 @@ def main():
         optimizer.zero_grad()
 
         # compute loss
-        #loss, tilted_loss = sample_loss(model, args.N_gd, device)
         loss, tilted_loss = sample_loss_vect(model, args.N_gd, device)
         print('{:d}, {:2.3f}'.format(update, loss))
 
         # compute gradients
-        tilted_loss.backward(retain_graph=True)
+        tilted_loss.backward()
 
         # update parameters
         optimizer.step()
@@ -77,7 +76,7 @@ def main():
 def save_nn_coefficients(thetas):
     from mds.utils import make_dir_path
     import os
-    dir_path = 'mds/data/testing_1d_gd_nn'
+    dir_path = 'mds/data/testing_nd_gd_nn'
     make_dir_path(dir_path)
     file_path = os.path.join(dir_path, 'gd.npz')
     np.savez(
@@ -85,70 +84,38 @@ def save_nn_coefficients(thetas):
         thetas=thetas,
     )
 
-def double_well_1d_gradient(x):
-    alpha = 1
-    return 4 * alpha * x * (x**2 - 1)
+def double_well_nd_gradient_vect(x):
+    assert x.ndim == 2, ''
+    N = x.shape[0]
+    n = x.shape[1]
+    grad = np.empty((N, n))
+    for i in range(n):
+        grad[:, i] = 4 * x[:, i] * (np.power(x[:, i], 2) - 1)
+    return grad
 
-def sample_loss(model, N, device):
-    beta = 1
+def is_in_ts(x):
+    is_in_target_set = x > 1
 
-    dt = 0.01
-    k_max = 100000
-
-    loss = np.zeros(N)
-    tilted_loss = torch.empty(N).to(device)
-
-    for i in np.arange(N):
-
-        # initialize trajectory
-        xt_traj = -1.
-        a_traj_tensor = torch.zeros(1).to(device)
-        b_traj_tensor = torch.zeros(1).to(device)
-        c_traj_tensor = torch.zeros(1).to(device)
-
-        for k in np.arange(1, k_max + 1):
-
-            # Brownian increment
-            dB = np.sqrt(dt) * np.random.normal(0, 1)
-
-            # control
-            xt_traj_tensor = torch.tensor([xt_traj], dtype=torch.float).to(device)
-            ut_traj_tensor = model.forward(xt_traj_tensor).to(device)
-            ut_traj_tensor_det = ut_traj_tensor.detach()
-            ut_traj = ut_traj_tensor_det.numpy()[0]
-
-            # sde update
-            drift = (- double_well_1d_gradient(xt_traj) + np.sqrt(2) * ut_traj) * dt
-            diffusion = np.sqrt(2 / beta) * dB
-            xt_traj += drift + diffusion
-
-            # update statistics
-            a_traj_tensor = a_traj_tensor + ut_traj_tensor_det * ut_traj_tensor * dt
-            b_traj_tensor = b_traj_tensor + (1 + 0.5 * (ut_traj_tensor_det ** 2)) * dt
-            c_traj_tensor = c_traj_tensor - np.sqrt(beta) * dB * ut_traj_tensor
-
-            # stop if xt_traj in target set
-            if xt_traj >= 1:
-
-                # save loss and tilted loss
-                loss[i] = b_traj_tensor.numpy()
-                tilted_loss[i] = a_traj_tensor - b_traj_tensor * c_traj_tensor
-                break
-
-    return np.mean(loss), torch.mean(tilted_loss)
-
+    if is_in_target_set.all():
+        return True
+    else:
+        return False
 
 def sample_loss_vect(model, N, device):
+    assert model.d_in == model.d_out, ''
+
+    n = model.d_in
+
     beta = 1
 
     dt = 0.01
     k_max = 100000
 
     loss = np.zeros(N)
-    tilted_loss = torch.zeros(N).to(device)
+    tilted_loss = torch.zeros(N)
 
     # initialize trajectory
-    xt = np.full(N, -1.).reshape(N, 1)
+    xt = np.full(N * n, -1.).reshape(N, n)
     a_tensor = torch.zeros(N).to(device)
     b_tensor = torch.zeros(N).to(device)
     c_tensor = torch.zeros(N).to(device)
@@ -158,24 +125,29 @@ def sample_loss_vect(model, N, device):
     for k in np.arange(1, k_max + 1):
 
         # Brownian increment
-        dB = np.sqrt(dt) * np.random.normal(0, 1, N).reshape(N, 1)
+        dB = np.sqrt(dt) * np.random.normal(0, 1, N * n).reshape(N, n)
         dB_tensor = torch.tensor(dB, requires_grad=False, dtype=torch.float32).to(device)
 
         # control
-        xt_tensor = torch.tensor(xt, dtype=torch.float).to(device)
-        ut_tensor = model.forward(xt_tensor).to(device)
+        xt_tensor = torch.tensor(xt, dtype=torch.float)
+        ut_tensor = model.forward(xt_tensor)
         ut_tensor_det = ut_tensor.detach()
         ut = ut_tensor_det.numpy()
 
         # sde update
-        drift = (- double_well_1d_gradient(xt) + np.sqrt(2) * ut) * dt
-        diffusion = np.sqrt(2 / beta) * dB
+        drift = (- double_well_nd_gradient_vect(xt) + np.sqrt(2) * ut) * dt
+        diffusion = np.dot(dB, np.sqrt(2 / beta) * np.eye(n))
         xt += drift + diffusion
 
         # update statistics
-        a_tensor = a_tensor + (ut_tensor_det * ut_tensor * dt).reshape(N,)
-        b_tensor = b_tensor + ((1 + 0.5 * (ut_tensor_det ** 2)) * dt).reshape(N,)
-        c_tensor = c_tensor - (np.sqrt(beta) * dB_tensor * ut_tensor).reshape(N,)
+        a_tensor = a_tensor \
+                 + torch.tensordot(ut_tensor_det, ut_tensor, dims=([1],[1])).diagonal() * dt
+
+        ut_norm_det = torch.linalg.norm(ut_tensor_det, axis=1)
+        b_tensor = b_tensor + ((1 + 0.5 * (ut_norm_det ** 2)) * dt).reshape(N,)
+
+        c_tensor = c_tensor \
+                 - np.sqrt(beta) * torch.tensordot(ut_tensor, dB_tensor, dims=([1], [1])).diagonal()
 
         # get indices of trajectories which are new to the target set
         idx = get_idx_new_in_ts(xt, been_in_target_set)
@@ -198,13 +170,29 @@ def sample_loss_vect(model, N, device):
     return np.mean(loss), torch.mean(tilted_loss)
 
 def get_idx_new_in_ts(x, been_in_target_set):
-    is_in_target_set = x > 1
+    # get num of trajectories and dimension n
+    N = x.shape[0]
+    n = x.shape[1]
 
+    # assume trajectories are in the target set
+    is_in_target_set = np.repeat([True], N)
+    for i in range(n):
+        is_not_in_target_set_i_axis_idx = np.where(x[:, i] < 1)[0]
+
+        # if they are NOT in the target set change flag
+        is_in_target_set[is_not_in_target_set_i_axis_idx] = False
+
+        # break if none of them is in the target set
+        if is_in_target_set.any() == False:
+            break
+
+    # indices of trajectories new in the target set
     idx = np.where(
-            (is_in_target_set == True) &
-            (been_in_target_set == False)
+        (is_in_target_set == True) &
+        (been_in_target_set == False)
     )[0]
 
+    # update list of indices whose trajectories have been in the target set
     been_in_target_set[idx] = True
 
     return idx
