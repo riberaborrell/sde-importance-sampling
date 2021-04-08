f938c3e0fb4c0de41017273f2c124f6cb6197991
eborrell <ribera.borrell@me.com>
2021-04-08 16:59:36 +0200

    create Makefile + update setup

---
+++ Makefile
@@ -0,0 +1,23 @@
+help:
+	@echo "---------------HELP-----------------"
+	@echo "To create venv with required packages type 'make venv'"
+	@echo "To create venv with developer packages type 'make develop'"
+	@echo "To run the tests of the project type 'make tests'"
+	@echo "To clean the virtual environment type 'make clean'"
+	@echo "------------------------------------"
+
+venv:
+	python3.7 -m venv venv
+	venv/bin/pip install --upgrade pip
+	venv/bin/pip install --upgrade setuptools
+	venv/bin/pip install -e .
+
+develop: venv
+	venv/bin/pip install -e .[test]
+
+tests: venv
+	venv/bin/pytest 
+
+clean:
+	rm -rf venv
+	rm -rf *.egg-info
--- setup.py
+++ setup.py
@@ -2,18 +2,38 @@ from setuptools import setup, find_packages
 
 import os
 
+
+with open('VERSION', 'r') as f:
+    VERSION = f.read().strip('\n')
+
 _dir = os.path.dirname(os.path.realpath(__file__))
 
-with open("VERSION", "r") as f:
-    VERSION = f.read().strip("\n")
+with open(os.path.join(_dir, 'README.md'), 'r') as f:
+    README = f.read()
+
 
 setup(
-    name="mds",
+    name='mds',
     version=VERSION,
-    description="Overdamped Langevin Importance Sampling",
-    url="https://github.com/eborrell/mds",
-    author="Enric Ribera Borrell",
-    author_email="ribera.borrell@me.com",
+    description='Overdamped Langevin Importance Sampling',
+    long_description_content_type='text/markdown',
+    long_description=README,
+    classifiers=[],
+    url='https://github.com/eborrell/mds',
+    license='GNU General Public License V3',
+    author='Enric Ribera Borrell',
+    author_email='ribera.borrell@me.com',
+    keywords=[],
+    zip_safe=True,
     include_package_data=True,
     packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'torch',
+    ],
+    extras_require={
+        'test': ['pytest'],
+    },
 )
