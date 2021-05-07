1013e7d6f2fa29a560eab3d45b1f1f05312df064
eborrell <ribera.borrell@me.com>
2021-05-07 14:05:19 +0200

    minor

--- mds/base_parser_nd.py
+++ mds/base_parser_nd.py
@@ -133,7 +133,7 @@ def get_base_parser():
     parser.add_argument(
         '--theta',
         dest='theta',
-        choices=['random', 'null', 'meta', 'hjb'],
+        choices=['random', 'null', 'meta', 'flat', 'hjb'],
         default='null',
         help='Type of control. Default: null',
     )
