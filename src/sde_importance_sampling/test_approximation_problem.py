43246079c721aae18565b733c4a1e5fe0d800f16
eborrell <ribera.borrell@fu-berlin.de>
2022-01-31 11:55:23 +0100

    #54 testing

--- src/sde_importance_sampling/test_approximation_problem.py
+++ src/sde_importance_sampling/test_approximation_problem.py
@@ -19,6 +19,154 @@ def get_parser():
     parser = get_base_parser()
     return parser
 
+def classic_training_algorithm(sde, ansatz, func, n_iterations_lim, N_train, lr):
+    '''
+    '''
+    # model
+    model = func.model
+
+    # preallocate losses
+    losses_train = np.empty(n_iterations_lim)
+    losses_test = np.empty(n_iterations_lim)
+
+    # define mean square error loss
+    loss = nn.MSELoss()
+
+    # define optimizer
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=lr,
+    )
+
+    # get data points
+    sde.discretize_domain(h=0.1)
+    data = sde.get_flat_domain_h()
+    N_data = data.shape[0]
+
+    # split into training and test data
+    N_train = int(0.75 * N_data)
+    np.random.shuffle(data)
+    x_train = data[:N_train]
+    x_test = data[N_train:]
+    N_test = x_test.shape[0]
+
+    # convert to tensors
+    x_train_tensor = torch.tensor(x_train, requires_grad=False, dtype=torch.float32)
+    x_test_tensor = torch.tensor(x_test, requires_grad=False, dtype=torch.float32)
+
+    # evaluation
+    target_train = ansatz.control(x_train)
+    target_test = ansatz.control(x_test)
+
+    # tensorize target function evaluation
+    target_train_tensor = torch.tensor(
+        target_train,
+        requires_grad=False,
+        dtype=torch.float32,
+    )
+    target_test_tensor = torch.tensor(
+        target_test,
+        requires_grad=False,
+        dtype=torch.float32,
+    )
+
+    for i in np.arange(n_iterations_lim):
+
+        # evaluate nn control at the training data
+        inputs_train = model.forward(x_train_tensor)
+        inputs_test = model.forward(x_test_tensor)
+
+        # compute loss
+        output_train = loss(inputs_train, target_train_tensor)
+        output_test = loss(inputs_test, target_test_tensor)
+        if i % 100 == 0:
+            msg = 'it.: {:d}, loss (train): {:2.3e}, loss (test): {:2.3e}' \
+                  ''.format(i, output_train, output_test)
+            print(msg)
+
+        # save loss
+        losses_train[i] = output_train.detach().numpy()
+        losses_test[i] = output_test.detach().numpy()
+
+        # compute gradients
+        output_train.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # reset gradients
+        optimizer.zero_grad()
+
+    print('it.: {:d}, loss (train): {:2.3e}, loss (test): {:2.3e} \n' \
+          ''.format(i, output_train, output_test))
+
+    # number of iterations used
+    n_iterations = i
+    return losses_train, losses_test
+
+
+def alternative_training_algorithm(sde, ansatz, func, n_iterations_lim, N_train, lr):
+    '''
+    '''
+    # model
+    model = func.model
+
+    # preallocate losses
+    losses_train = np.empty(n_iterations_lim)
+
+    # define mean square error loss
+    loss = nn.MSELoss()
+
+    # define optimizer
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=lr,
+    )
+
+    # multivariate normal sampling
+    mean = ansatz.means[0]
+    cov = ansatz.cov
+
+    for i in np.arange(n_iterations_lim):
+
+        # sample training data
+        #x = sde.sample_domain_uniformly(N=N_train, subset=np.full((sde.n, 2), [-2, 0]))
+        #x = sde.sample_domain_ith_coordinate_uniformly(i=0, N=N_train)
+        x = sde.sample_multivariate_normal(mean=mean, cov=cov, N=N_train)
+        x_tensor = torch.tensor(x, requires_grad=False, dtype=torch.float32)
+
+        # evaluate target function
+        target = ansatz.control(x)
+        target_tensor = torch.tensor(target, requires_grad=False, dtype=torch.float32)
+
+        # evaluate model
+        inputs = model.forward(x_tensor)
+
+        # compute mse loss
+        output = loss(inputs, target_tensor)
+
+        if i % 100 == 0:
+            print('it.: {:d}, loss: {:2.3e}'.format(i, output))
+
+        # save loss
+        losses_train[i] = output.detach().numpy()
+
+        # compute gradients
+        output.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # reset gradients
+        optimizer.zero_grad()
+
+    print('it.: {:d}, loss: {:2.3e}\n'.format(i, output))
+    return losses_train
+
+def get_dir_path(training_algorithm):
+    dir_path = 'data/testing_approximation_problem/{}/'.format(training_algorithm)
+    return dir_path
+
 def get_file_name(n, n_layers, d_layer, n_iterations_lim, N_train, lr):
     file_name = ''
     file_name += 'n-{:d}_'.format(n)
@@ -30,15 +178,13 @@ def get_file_name(n, n_layers, d_layer, n_iterations_lim, N_train, lr):
     file_name += '.npz'
     return file_name
 
-def save(file_name, files_dict):
-    dir_path = 'data/testing_approximation_problem'
+def save(dir_path, file_name, files_dict):
     make_dir_path(dir_path)
 
     file_path = os.path.join(dir_path, file_name)
     np.savez(file_path, **files_dict)
 
-def load(file_name):
-    dir_path = 'data/testing_approximation_problem'
+def load(dir_path, file_name):
     make_dir_path(dir_path)
     file_path = os.path.join(dir_path, file_name)
     data = np.load(
@@ -56,6 +202,9 @@ def main():
     # nn architecture
     n_layers = args.n_layers
     d_layer = args.d_layer
+
+    # training algorithm
+    training_algorithm = args.train_alg
 
     # sgd
     n_iterations_lim = args.n_iterations_lim
@@ -81,15 +230,12 @@ def main():
         ansatz = GaussianAnsatz(n, args.beta, normalized=False)
 
         # set gaussian
-        means = 0. * np.ones((1, n))
+        means = -1. * np.ones((1, n))
         cov = 1 * np.eye(n)
         ansatz.set_given_ansatz_functions(means, cov)
 
         # set weights
         ansatz.theta = 1. * np.ones(1)
-
-        # preallocate losses
-        losses_train = np.empty(n_iterations_lim)
 
         # get function approximator
         d_innen_layers = [d_layer for i in range(n_layers)]
@@ -103,51 +249,17 @@ def main():
         func = FunctionApproximation(
             target_function='control',
             model=model,
-            initialization='random',
-            training_algorithm='alternative',
         )
 
-        # define mean square error loss
-        loss = nn.MSELoss()
 
-        # define optimizer
-        optimizer = optim.Adam(
-            model.parameters(),
-            lr=lr,
-        )
-
-        for i in np.arange(n_iterations_lim):
-
-            # sample training data
-            x = sde.sample_domain_uniformly(N=N_train)
-            x_tensor = torch.tensor(x, requires_grad=False, dtype=torch.float32)
-
-            # evaluate target function
-            target = ansatz.control(x)
-            target_tensor = torch.tensor(target, requires_grad=False, dtype=torch.float32)
-
-            # evaluate model
-            inputs = model.forward(x_tensor)
-
-            # compute mse loss
-            output = loss(inputs, target_tensor)
-
-            if i % 100 == 0:
-                print('it.: {:d}, loss: {:2.3e}'.format(i, output))
-
-            # save loss
-            losses_train[i] = output.detach().numpy()
-
-            # compute gradients
-            output.backward()
-
-            # update parameters
-            optimizer.step()
-
-            # reset gradients
-            optimizer.zero_grad()
-
-        print('it.: {:d}, loss: {:2.3e}\n'.format(i, output))
+        # classic training algorithm
+        if training_algorithm == 'classic':
+            losses_train, losses_test = classic_training_algorithm(sde, ansatz, func,
+                                                                   n_iterations_lim, N_train, lr)
+        # alternative training algorithm
+        elif training_algorithm == 'alternative':
+            losses_train = alternative_training_algorithm(sde, ansatz, func,
+                                                          n_iterations_lim, N_train, lr)
 
         # inputs
         x = np.empty((6, sde.n))
@@ -184,15 +296,20 @@ def main():
             'ct': ct,
         }
 
+        if training_algorithm == 'classic':
+            files_dict['losses_test'] = losses_test
+
+        dir_path = get_dir_path(training_algorithm)
         file_name = get_file_name(n, n_layers, d_layer, n_iterations_lim, N_train, lr)
-        save(file_name, files_dict)
+        save(dir_path, file_name, files_dict)
 
     # load already computed approximation problem
     if args.load:
 
         # load npz files
+        dir_path = get_dir_path(training_algorithm)
         file_name = get_file_name(n, n_layers, d_layer, n_iterations_lim, N_train, lr)
-        data = load(file_name)
+        data = load(dir_path, file_name)
 
         # get target function and model at x
         x = data['x']
@@ -209,9 +326,15 @@ def main():
 
         # plot training losses
         n_iterations_lim = data['n_iterations_lim']
-        losses_train = data['losses_train']
         x = np.arange(n_iterations_lim)
-        plt.semilogy(x, losses_train)
+        losses_train = data['losses_train']
+        if training_algorithm == 'classic':
+            losses_test = data['losses_test']
+
+        plt.semilogy(x, losses_train, label='train')
+        if training_algorithm == 'classic':
+            plt.semilogy(x, losses_test, label='test')
+        plt.legend()
         plt.show()
 
 
