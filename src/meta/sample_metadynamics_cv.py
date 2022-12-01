6c2f922766a00cd0947c45a74e4a1c5aaa681d33
Enric Ribera Borrell <ribera.borrell@me.com>
2022-12-01 18:13:42 +0100

    minor

--- src/meta/sample_metadynamics_cv.py
+++ src/meta/sample_metadynamics_cv.py
@@ -110,57 +110,8 @@ def main():
         if not meta.load():
             return
 
-    # set ansatz and theta
-    meta.sample.ansatz.set_given_ansatz_functions(
-            means=meta.means,
-            sigma_i=meta.sigma_i,
-    )
-    meta.sample.ansatz.theta = - meta.omegas / meta.sde.sigma
-    meta.sample.is_controlled = True
-
-    x = np.random.rand(10, 20)
-    y = meta.sample.ansatz.mvn_pdf_gradient_extended_basis(x)
-
-
-    #meta.compute_means_distance()
     if args.do_report:
         meta.write_report()
-
-    if args.do_plots:
-
-        # n gaussians added for each trajectory
-        meta.plot_n_gaussians()
-
-        # time steps used for each trajectory
-        meta.plot_time_steps()
-
-        # 1d plots
-        if sde.d == 1:
-            idx_traj_with_updates = [i for i in range(args.K_meta) if meta.ms[i] != 0]
-            for i in idx_traj_with_updates:
-                meta.plot_1d_updates(i=i)
-            #meta.plot_1d_update(i=i, update=1)
-
-        # 2d plots
-        elif sde.d == 2:
-            meta.plot_2d_means()
-            meta.plot_2d_update()
-            meta.plot_nd_ith_coordinate_update(i=0)
-            meta.plot_nd_ith_coordinate_update(i=1)
-
-        # 3d plots
-        elif sde.d == 3:
-            meta.plot_3d_means()
-            meta.plot_nd_ith_coordinate_update(i=0)
-            meta.plot_nd_ith_coordinate_update(i=1)
-
-        # nd plots
-        else:
-            #meta.plot_nd_means()
-            meta.plot_nd_ith_coordinate_update(i=0)
-            meta.plot_nd_ith_coordinate_update(i=1)
-
-        #meta.compute_probability_sampling_in_support()
 
 
 if __name__ == "__main__":
